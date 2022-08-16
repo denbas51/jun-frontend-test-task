2fc32d133aff55b3c32a9ea295eff3e9e8c7cd6b
Denys <denys@Denyss-MacBook-Air.local>
2022-08-16 08:48:04 +0300

    linked server to the front

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import io from "socket.io-client"
+import { useEffect } from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React.
-        </a>
-      </header>
-    </div>
-  );
+  const socket = io.connect("http://localhost:3002")
+  socket.on("connect", () => console.log(socket.connected))
+  socket.emit("start")
+  useEffect(() => {
+    socket.on("ticker", (round) => {
+      console.log(round)
+    })
+  }, [socket])
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
